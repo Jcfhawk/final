c1f0feba5b95b54c987e0bee0e7f6081cb8043c4
Jack Hawkins <Jcfhawk81@gmail.com>
2026-08-31 18:29:00 -0500

    Added End of day trading

--- ml.py
+++ ml.py
@@ -1,9 +1,7 @@
 import socket
 import json
 
-
-def send_message(message, host="192.168.1.2", port=8002):
+def send_message(message, host="169.254.45.67", port=8002):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(json.dumps(message).encode())
-
